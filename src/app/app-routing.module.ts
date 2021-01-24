bcda71437a442e136400f3b4e55a57e5b03032be
bhadreshdudhat <bhadreshdudhat@gmail.com>
2021-01-24 14:34:36 -0600

    Routing 2 :  add guard 1 : create  AuthGuard and apply it to Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,11 +10,13 @@ import { MemberDetailComponent } from './members/member-detail/member-detail.com
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {
     path: '',
+    canActivate: [AuthGuard],
     children: [
       {path: 'members', component: MemberListComponent},
       {path: 'members/:username', component: MemberDetailComponent},
