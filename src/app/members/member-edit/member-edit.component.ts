698b404e4be5ca23055e0cdfa513af16cf34235f
bhadreshdudhat <bhadreshdudhat@gmail.com>
2021-01-26 20:01:47 -0600

    add Edit Member functionality (corresponding WEBAPI commit : add id field to User model)

--- src/app/members/member-edit/member-edit.component.ts
+++ src/app/members/member-edit/member-edit.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { take } from 'rxjs/operators';
+import { Member } from 'src/app/_models/member';
+import { User } from 'src/app/_models/user';
+import { AccountService } from 'src/app/_services/account.service';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-edit',
@@ -6,10 +13,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-edit.component.scss']
 })
 export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm') editForm: NgForm;
+  member: Member;
+  user: User;
+  @HostListener('window:beforeunload', ['$event']) unloadNotification($event: any) {
+    if (this.editForm.dirty) {
+      $event.returnValue = true;
+    }
+  }
 
-  constructor() { }
+  constructor(private accountService: AccountService, private memberService: MembersService,
+    private toastr: ToastrService) {
+      this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user = user);
+  }
 
   ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember() {
+    this.memberService.getMember(this.user.id).subscribe(member => {
+      this.member = member;
+    })
+  }
+
+  updateMember() {
+    this.memberService.updateMember(this.member).subscribe(() => {
+      this.toastr.success('Profile updated successfully');
+      this.editForm.reset(this.member);
+    })
   }
 
 }
